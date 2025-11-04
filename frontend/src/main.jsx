f8ed54d1b548405b87fdbbe3abb3f01a4e5ed284
khushi-b-patel <kp1032@tamu.edu>
2025-11-03 20:18:49 -0600

    Hamburger icon fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './styles.css'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render
   <React.StrictMode>
     <App />
   </React.StrictMode>
